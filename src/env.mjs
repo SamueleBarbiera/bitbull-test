48c464cce2197612df9c318fb4246c212f9bfcdb
samuelebarbiera <barbierasamuele01@gmail.com>
2023-09-06 13:59:30 +0200

    :art:

--- src/env.mjs
+++ src/env.mjs
@@ -16,17 +16,18 @@ export const env = createEnv({
     experimental__runtimeEnv: {
         NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
         NEXT_PUBLIC_API: process.env.NEXT_PUBLIC_API,
+        NODE_ENV: process.env.NODE_ENV,
     },
     // converts the variable to a boolean if it's 1 or 0
     skipValidation: !!process.env.SKIP_ENV_VALIDATION,
     onValidationError: (error) => {
-        console.error("❌ Invalid environment variables:", error.flatten().fieldErrors);
-        throw new Error("❌ Invalid environment variables", error.flatten().fieldErrors);
+        console.error("❌ Invalid environment variables: ", error.flatten().fieldErrors);
+        throw new Error("❌ Invalid environment variables: ", error.flatten().fieldErrors);
     },
     // Called when server variables are accessed on the client.
     onInvalidAccess: (variable) => {
-        console.error("❌ Invalid access variable:", variable);
-        throw new Error("❌ Attempted to access a server-side environment variable on the client");
+        console.error("❌ Invalid access variable: ", variable);
+        throw new Error("❌ Attempted to access a server-side environment variable on the client: ", variable);
     },
 
     isServer: typeof window === "undefined",
